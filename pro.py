3c89a32f59ff176402c1d13f4e46ac9453b331b3
Aurélio Martins Filho <Aureliofilhotj@gmail.com>
2023-07-24 19:22:24 -0300

    Acrescentando Readme.md

---
+++ pro.py
@@ -0,0 +1,6 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import QIcon
+import sys
+
+def nome_maiusculo(self):
+    self.texto2.setText('AURÉLIO FILHO')
--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QIcon
 import sys
+import pro
 class JanelaPrincipal(QWidget):
     def __init__(self):
         super().__init__()
@@ -9,29 +10,43 @@ class JanelaPrincipal(QWidget):
         self.setWindowIcon(QIcon('/home/aurelio/Imagens/6c1e5af3b28948b4ef2219e4f84ebc25.jpg'))
         self.Interface()
 
+        #self.show()# tenho que ver o codigo completo
     def Interface(self):
-        texto1 = QLabel('Olá Mundo!!!', self)
-        texto1.resize(250, 250)
-        texto1.move(100, 5)
+        texto1 = QLabel('Login', self)
+        texto1.move(40, 50)
 
-        botao1 = QPushButton('Sair', self)
+        botao1 = QPushButton('Sair'.upper(), self)
         botao1.move(100, 200)
         botao1.clicked.connect(self.sair)
 
+        self.caixa_texto1 = QLineEdit(self)
+        self.caixa_texto1.setPlaceholderText('Digite seu nome de usuário')
+        self.caixa_texto1.move(90, 48)
 
+        texto2 = QLabel('Senha: ', self)
+        texto2.move(40, 75)
 
-        texto2 = QLabel('Aurélio Filho', self)
-        texto2.resize(250, 250)
-        texto2.move(200, 5)
+        self.caixa_texto2 = QLineEdit(self)
+        self.caixa_texto2.setPlaceholderText('Digite sua senha')
+        self.caixa_texto2.setEchoMode(QLineEdit.EchoMode.Password)
+        self.caixa_texto2.move(90, 72)
 
+        botao2 = QPushButton('Entrar', self)
+        botao2.clicked.connect(self.salva_dados)
+        botao2.move(117, 98)
 
-
+        self.show()# tenho que ver o codigo completo
     def sair(self):
         sys.exit(qt.exec())
 
+    def salva_dados(self):
+        base = []
+        base.append(self.caixa_texto1.text())
+        base.append(self.caixa_texto2.text())
+        print(base)
 
-        self.show()
-
+    # def Invertido(self):
+    #     self.texto1.setText('!!!odnum álO'.upper())
 
 qt = QApplication(sys.argv)
 app = JanelaPrincipal()
